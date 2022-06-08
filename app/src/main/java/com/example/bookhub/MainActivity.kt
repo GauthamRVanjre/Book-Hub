b4844f7807e31e855de11be46a5e9ee958af9943
unknown <vanjregautham@gmail.com>
2022-06-08 19:34:50 +0530

    added click listeners to the menu items

--- app/src/main/java/com/example/bookhub/MainActivity.kt
+++ app/src/main/java/com/example/bookhub/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.FrameLayout
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -34,6 +35,27 @@ class MainActivity : AppCompatActivity() {
         val actionBarDrawerToggle = ActionBarDrawerToggle(this@MainActivity,drawerLayout,R.string.open_drawer,R.string.close_drawer)
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
+
+        //adding click istener for menu items
+        navigationView.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+                R.id.dashboard -> {
+                    Toast.makeText(this,"dashboard item is clicked",Toast.LENGTH_SHORT).show()
+                }
+                R.id.favorites -> {
+                    Toast.makeText(this,"favorites is clicked",Toast.LENGTH_SHORT).show()
+                }
+                R.id.profile -> {
+                    Toast.makeText(this,"profile is clicked", Toast.LENGTH_LONG).show()
+                }
+                R.id.aboutApp -> {
+                    Toast.makeText(this,"about app is clicked",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            return@setNavigationItemSelectedListener true
+        }
     }
 
     fun setUpToolBar(){
